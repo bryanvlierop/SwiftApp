5df8c7aa11d95ac949d1db0a3fa8df88dff72005
Bryan van Lierop <blierop1@avans.nl>
2019-05-31 19:20:28 +0200

    Init

---
+++ SwiftApp/SwiftApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftApp
+//
+//  Created by Bryan van Lierop on 31/05/2019.
+//  Copyright © 2019 Bryan van Lierop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
